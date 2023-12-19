fd4e8ed345d648cb0677db9d520102cdc3eb6ed1
Aman Kumar Bairagi <amanbairagi1089@gmail.com>
2023-12-19 21:49:10 +0530

    Redux Tolkit has been added

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { NextUIProvider } from '@nextui-org/react'
+import { Provider } from 'react-redux'
+import store from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <NextUIProvider>
-        <App />
-      </NextUIProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <NextUIProvider>
+          <App />
+        </NextUIProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
