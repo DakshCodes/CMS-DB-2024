04270922eba39898a6421e8f07322518b3b8823d
dakshsinghk2524@gmail.com[200~you@example.com~ <you@example.com>
2023-12-23 18:13:34 +0530

    add setting frontend

--- src/components/ui/DropDown/DropDown.jsx
+++ src/components/ui/DropDown/DropDown.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
 import './DropDown.css'
-const DropDown = () => {
 
+const DropDown = ({ name }) => {
 
   return (
     <div className={`main-dropdown `}>
       <div className="header-logo">
-        <img src='https://i.pinimg.com/564x/83/f7/39/83f73922730db28d9b8c3659df12b1ad.jpg' alt="" />
+        <h1 className='font font-black text-2xl '>
+          PERMISSION
+          {/* {name} */}
+        </h1>
+        {/* <img src={"https://i.pinimg.com/originals/41/f8/90/41f89016c529166a1cca6ec882665449.gif"} alt="" /> */}
       </div>
     </div>
   )
