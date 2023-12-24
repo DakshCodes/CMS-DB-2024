900240add9ab04d22df3cbea58f00affb9871a31
dakshsinghk2524@gmail.com[200~you@example.com~ <you@example.com>
2023-12-24 21:08:24 +0530

    product data resolved-4

--- src/pages/BillBoard/BIllBoard.jsx
+++ src/pages/BillBoard/BIllBoard.jsx
@@ -4,6 +4,25 @@ import Butoon from '../../components/ui/Butoon'
 import Heading from '../../components/ui/Heading'
 
 const BIllBoard = () => {
+    const columns = [
+        { name: "ID", uid: "id", sortable: true },
+        { name: "NAME", uid: "name", },
+        { name: "DATE", uid: "date" },
+        { name: "ACTIONS", uid: "actions" },
+    ];
+
+    const users = [
+        {
+            id: 1,
+            name: "Mens",
+            date: "2023-13-12",
+        },
+        {
+            id: 2,
+            name: "Womens",
+            date: "2021-10-12",
+        },
+    ];
     return (
         <>
             <div className="flex items-center justify-between  border-b mx-4 md:mx-10 pb-3 py-5">
@@ -12,7 +31,7 @@ const BIllBoard = () => {
                 </div>
                 <Butoon title={"Add New"} />
             </div >
-            <DataTable />
+            <DataTable data={users} columnss={columns} />
         </>
     )
 }
--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -5,6 +5,25 @@ import Butoon from '../../components/ui/Butoon'
 import Heading from '../../components/ui/Heading'
 
 const Orders = () => {
+    const columns = [
+        { name: "ID", uid: "id", sortable: true },
+        { name: "NAME", uid: "name", },
+        { name: "DATE", uid: "date" },
+        { name: "ACTIONS", uid: "actions" },
+    ];
+
+    const users = [
+        {
+            id: 1,
+            name: "Mens",
+            date: "2023-13-12",
+        },
+        {
+            id: 2,
+            name: "Womens",
+            date: "2021-10-12",
+        },
+    ];
     return (
         <>
             <div className="flex items-center justify-between  border-b mx-4 md:mx-10 pb-3 py-5">
@@ -12,7 +31,7 @@ const Orders = () => {
                     <Heading title={`Orders (0)`} description="Manage Orders for your store" />
                 </div>
             </div >
-            <DataTable />
+            <DataTable data={users} columnss={columns} />
         </>
     )
 }
