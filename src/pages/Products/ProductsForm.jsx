2d7cce8133438faf7ca8b2f21bc4337bb4382af2
Aman Kumar Bairagi <amanbairagi1089@gmail.com>
2023-12-29 21:03:34 +0530

    Attribute section tab added

--- src/pages/Products/ProductsForm.jsx
+++ src/pages/Products/ProductsForm.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Heading from '../../components/ui/Heading'
-import { Input, Button, Tabs, Tab, Card, Select, SelectItem, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure, } from "@nextui-org/react";
+import { Input, Button, Tabs, Tab, Card, Select, SelectItem, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure, Avatar, Chip, } from "@nextui-org/react";
 import Selected from '../../components/ui/Select';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
@@ -10,6 +10,7 @@ import toast from 'react-hot-toast';
 import { SetLoader } from "../../redux/loadersSlice";
 import { useNavigate, useParams } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import { GetAttributeData } from '../../apicalls/attributes';
 
 const ProductsForm = () => {
 
@@ -70,6 +71,28 @@ const ProductsForm = () => {
         }));
     };
 
+    const [attributeData, setAttributeData] = useState([]);
+
+    const getAttributeData = async () => {
+        try {
+            dispatch(SetLoader(true));
+            const response = await GetAttributeData();
+            dispatch(SetLoader(false));
+            if (response.success) {
+                setAttributeData(response.attributes);
+            } else {
+                throw new Error(response.message);
+            }
+        } catch (error) {
+            dispatch(SetLoader(false));
+            toast.error(error.message)
+        }
+    }
+
+    React.useEffect(() => {
+        getAttributeData();
+    }, [])
+
     const handleAttributeChange = (index, field, value) => {
         const updatedAttributes = [...attributes];
         updatedAttributes[index][field] = value;
@@ -97,6 +120,208 @@ const ProductsForm = () => {
         }));
     };
 
+    const users = [
+        {
+          id: 1,
+          name: "Tony Reichert",
+          role: "CEO",
+          team: "Management",
+          status: "active",
+          age: "29",
+          avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/male/1.png",
+          email: "tony.reichert@example.com",
+        },
+        {
+          id: 2,
+          name: "Zoey Lang",
+          role: "Tech Lead",
+          team: "Development",
+          status: "paused",
+          age: "25",
+          avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/1.png",
+          email: "zoey.lang@example.com",
+        },
+        {
+          id: 3,
+          name: "Jane Fisher",
+          role: "Sr. Dev",
+          team: "Development",
+          status: "active",
+          age: "22",
+          avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/2.png",
+          email: "jane.fisher@example.com",
+        },
+        {
+          id: 4,
+          name: "William Howard",
+          role: "C.M.",
+          team: "Marketing",
+          status: "vacation",
+          age: "28",
+          avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/male/2.png",
+          email: "william.howard@example.com",
+        },
+        {
+          id: 5,
+          name: "Kristen Copper",
+          role: "S. Manager",
+          team: "Sales",
+          status: "active",
+          age: "24",
+          avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/3.png",
+          email: "kristen.cooper@example.com",
+        },
+        {
+          id: 6,
+          name: "Brian Kim",
+          role: "P. Manager",
+          team: "Management",
+          age: "29",
+          avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/male/3.png",
+          email: "brian.kim@example.com",
+          status: "Active",
+        },
+        {
+          id: 7,
+          name: "Michael Hunt",
+          role: "Designer",
+          team: "Design",
+          status: "paused",
+          age: "27",
+          avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/male/4.png",
+          email: "michael.hunt@example.com",
+        },
+        {
+          id: 8,
+          name: "Samantha Brooks",
+          role: "HR Manager",
+          team: "HR",
+          status: "active",
+          age: "31",
+          avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/4.png",
+          email: "samantha.brooks@example.com",
+        },
+        {
+          id: 9,
+          name: "Frank Harrison",
+          role: "F. Manager",
+          team: "Finance",
+          status: "vacation",
+          age: "33",
+          avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/male/5.png",
+          email: "frank.harrison@example.com",
+        },
+        {
+          id: 10,
+          name: "Emma Adams",
+          role: "Ops Manager",
+          team: "Operations",
+          status: "active",
+          age: "35",
+          avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/5.png",
+          email: "emma.adams@example.com",
+        },
+        {
+          id: 11,
+          name: "Brandon Stevens",
+          role: "Jr. Dev",
+          team: "Development",
+          status: "active",
+          age: "22",
+          avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/male/7.png",
+          email: "brandon.stevens@example.com",
+        },
+        {
+          id: 12,
+          name: "Megan Richards",
+          role: "P. Manager",
+          team: "Product",
+          status: "paused",
+          age: "28",
+          avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/7.png",
+          email: "megan.richards@example.com",
+        },
+        {
+          id: 13,
+          name: "Oliver Scott",
+          role: "S. Manager",
+          team: "Security",
+          status: "active",
+          age: "37",
+          avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/male/8.png",
+          email: "oliver.scott@example.com",
+        },
+        {
+          id: 14,
+          name: "Grace Allen",
+          role: "M. Specialist",
+          team: "Marketing",
+          status: "active",
+          age: "30",
+          avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/8.png",
+          email: "grace.allen@example.com",
+        },
+        {
+          id: 15,
+          name: "Noah Carter",
+          role: "IT Specialist",
+          team: "I. Technology",
+          status: "paused",
+          age: "31",
+          avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/male/9.png",
+          email: "noah.carter@example.com",
+        },
+        {
+          id: 16,
+          name: "Ava Perez",
+          role: "Manager",
+          team: "Sales",
+          status: "active",
+          age: "29",
+          avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/9.png",
+          email: "ava.perez@example.com",
+        },
+        {
+          id: 17,
+          name: "Liam Johnson",
+          role: "Data Analyst",
+          team: "Analysis",
+          status: "active",
+          age: "28",
+          avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/male/11.png",
+          email: "liam.johnson@example.com",
+        },
+        {
+          id: 18,
+          name: "Sophia Taylor",
+          role: "QA Analyst",
+          team: "Testing",
+          status: "active",
+          age: "27",
+          avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/11.png",
+          email: "sophia.taylor@example.com",
+        },
+        {
+          id: 19,
+          name: "Lucas Harris",
+          role: "Administrator",
+          team: "Information Technology",
+          status: "paused",
+          age: "32",
+          avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/male/12.png",
+          email: "lucas.harris@example.com",
+        },
+        {
+          id: 20,
+          name: "Mia Robinson",
+          role: "Coordinator",
+          team: "Operations",
+          status: "active",
+          age: "26",
+          avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/12.png",
+          email: "mia.robinson@example.com",
+        },
+      ];
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -151,7 +376,7 @@ const ProductsForm = () => {
             [name]: value,
         });
     };
-    
+
 
     const uploadImage = async (e) => {
         e.preventDefault();
@@ -176,38 +401,6 @@ const ProductsForm = () => {
     };
 
 
-    const users = [
-        {
-            id: 1,
-            name: "Tony Reichert",
-            role: "CEO",
-            team: "Management",
-            status: "active",
-            age: "29",
-            avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/male/1.png",
-            email: "tony.reichert@example.com",
-        },
-        {
-            id: 2,
-            name: "Zoey Lang",
-            role: "Tech Lead",
-            team: "Development",
-            status: "paused",
-            age: "25",
-            avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/1.png",
-            email: "zoey.lang@example.com",
-        },
-        {
-            id: 3,
-            name: "Jane Fisher",
-            role: "Sr. Dev",
-            team: "Development",
-            status: "active",
-            age: "22",
-            avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/2.png",
-            email: "jane.fisher@example.com",
-        },
-    ];
 
     return (
         <div className="flex-col">
@@ -280,69 +473,61 @@ const ProductsForm = () => {
                             </Tab>
                             <Tab key="attributes" title="Attributes">
                                 {/* Attribute add model */}
-                                <Modal
-                                    isOpen={isOpen}
-                                    placement={"top-center"}
-                                    onOpenChange={onOpenChange}
-                                >
-                                    <ModalContent>
-                                        {(onClose) => (
-                                            <>
-                                                <ModalHeader className="flex flex-col gap-1">Create Attribute</ModalHeader>
-                                                <ModalBody>
-                                                    <Input size={'sm'}
-                                                        classNames={{
-                                                            label: "font-bold font-3",
-                                                            input: "font-bold font",
-                                                        }}
 
-                                                        type="text" label="Name" />
-                                                </ModalBody>
-                                                <ModalFooter>
-                                                    <Button color="danger" variant="light" onPress={onClose}>
-                                                        Close
-                                                    </Button>
-                                                    <Button className='bg-[#000] text-[#fff]' onPress={onClose}>
-                                                        Create
-                                                    </Button>
-                                                </ModalFooter>
-                                            </>
-                                        )}
-                                    </ModalContent>
-                                </Modal>
                                 <Card className='px-5 py-3 flex gap-5'>
-                                    <Button onPress={onOpen} className="font-sans max-w-[7rem] bg-[#000]  font-semibold text-[0.8rem]" color='primary'>
-                                        Add attribute
-                                    </Button>
                                     <div className='flex flex-col gap-8 '>
                                         {attributes?.map((attribute, index) => (
                                             <div key={index} className='flex gap-5 max-w-max'>
                                                 <Select
-                                                    items={users}
-                                                    placeholder="select attribute"
+                                                    items={attributeData}
+                                                    placeholder="Select Attribute"
                                                     labelPlacement="outside"
                                                     variant='faded'
-                                                    className="w-[10rem]  font font-bold"
-                                                    onSelect={(user) => handleAttributeChange(index, 'type', user.name)}
+                                                    className="w-[40rem]  font font-bold"
+                                                    onSelect={(attributeData) => handleAttributeChange(index, 'type', attributeData.name)}
                                                 >
-                                                    {(user) => (
-                                                        <SelectItem key={user.id} textValue={user.name}>
+                                                    {(attributeData) => (
+                                                        <SelectItem key={attributeData._id} textValue={attributeData.name}>
                                                             <div className="flex gap-2 items-center">
                                                                 <div className="flex flex-col">
-                                                                    <span className="text-small font-bold">{user.name}</span>
+                                                                    <span className="text-small font-bold">{attributeData.name}</span>
                                                                 </div>
                                                             </div>
                                                         </SelectItem>
                                                     )}
                                                 </Select>
-                                                <Input
-                                                    type="url"
-                                                    placeholder="value"
+                                                <Select
+                                                    items={users}
+                                                    variant='faded'
+                                                    isMultiline={true}
+                                                    selectionMode="multiple"
+                                                    placeholder="Select a user"
                                                     labelPlacement="outside"
-                                                    className="max-w-[10rem]  font font-bold"
-                                                    value={attribute.value}
-                                                    onChange={(e) => handleAttributeChange(index, 'value', e.target.value)}
-                                                />
+                                                    className="w-[40rem]  font font-bold"
+
+                                                    renderValue={(items) => {
+                                                        return (
+                                                            <div className="flex flex-wrap gap-2">
+                                                                {items.map((item) => (
+                                                                    <Chip key={item.key}>{item.data.name}</Chip>
+                                                                ))}
+                                                            </div>
+                                                        );
+                                                    }}
+                                                >
+                                                    {(user) => (
+                                                        <SelectItem key={user.id} textValue={user.name}>
+                                                            <div className="flex gap-2 items-center">
+                                                                <Avatar alt={user.name} className="flex-shrink-0" size="sm" src={user.avatar} />
+                                                                <div className="flex flex-col">
+                                                                    <span className="text-small">{user.name}</span>
+                                                                    <span className="text-tiny text-default-400">{user.email}</span>
+                                                                </div>
+                                                            </div>
+                                                        </SelectItem>
+                                                    )}
+                                                </Select>
+
                                                 {!index == 0 && <Button
                                                     isIconOnly
                                                     color="warning"
